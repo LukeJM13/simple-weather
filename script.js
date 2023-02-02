f53b1dfca689f294d259914e8b6f8110b3a1b70f
Luke Mcfarlane <lmcfarlane97@hotmail.com>
2023-02-02 14:37:05 +0000

    Simplyfied script and added comments

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+// Selecting DOM elements for use in script
 const form = document.querySelector(".top-banner form");
-let input = document.querySelector("input");
-let inputVal = "";
+const input = document.querySelector("input");
+const apiKey = "f8ac4b0a6e8232a4b87f3ab82c6f5931";
+
+// Submit button event
 form.addEventListener("submit", e => {
+  // Prevents spam of submit button
   e.preventDefault();
-  const apiKey = "f8ac4b0a6e8232a4b87f3ab82c6f5931";
+  const inputVal = input.value;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-  console.log("Test");
-  inputVal = input.value;
-  console.log(inputVal);
+  // Requests the data for given city from OpenWeather
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    // do stuff with the data 
-  console.log(data);
-  console.log(Math.round(data.main.temp));  
+    // If the user has entered a valid city
+    console.log(`The temperature in ${inputVal} is ${Math.round(data.main.temp)}°C`);
   })
+  // If the user has entered an invalid city
   .catch(() => {
-    msg.textContent = "Please search for a valid city 😩";
+    console.log("No city found");
   });
 });
